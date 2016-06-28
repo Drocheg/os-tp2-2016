741c7771fee834b3ca46e823b3b4af6e9a06d88f
Droche <you@example.com>
2016-06-28 16:27:51 -0300

    Game almost done. No msgQ. No scheduling

--- Userland/c/game.c
+++ Userland/c/game.c
@@ -14,8 +14,10 @@
 #define HORIZONTAL_SIZE 10
 #define VERTICAL_SIZE 10
 #define GAME_TICK 200
-#define JUMP_LAG 50
+#define JUMP_LAG 200
 #define OBSTACLE_LAG_MULTIPLIER 6
+#define NO_OBSTACLE_MULTIPLIER 5
+#define GAME_OVER_LAG 10000
 
 
 static uint64_t state;
@@ -56,7 +58,7 @@ void initGame(){
 void playGame(){
 	uint64_t loopTime=time();
 	while(1){
-		while(time()<loopTime+GAME_TICK);
+		while(time()<loopTime+GAME_TICK/10);
 		loopTime=time();
 		update();
 	//	yield();
@@ -67,8 +69,10 @@ void playGame(){
 void jump(){
 	if(state==STATE_AIR) return;
 	if(jumpForce>=VERTICAL_SIZE-3) return;
+	//print("F");
 	jumpForce+=1;
 	if(state==STATE_GROUND){
+		//print("S");
 		state=STATE_JUMPING;
 		jumpStartTime=time();
 	}
@@ -79,44 +83,58 @@ void update(){
 	uint64_t updateTime=time(); //Make all updates with the same time.
 	clearScreen();
 	//Character Update
-	if(STATE_JUMPING){
-		if(jumpStartTime+JUMP_LAG>updateTime){
+	if(state==STATE_JUMPING){
+		//print("J");
+		if(jumpStartTime+JUMP_LAG<updateTime){
 			state=STATE_AIR;
 		}
-		if(isPlayerJumping()){
-			jump();
-		}
 	}
-	if(STATE_AIR){
+	if(isPlayerJumping()){
+		jump();
+	}
+	if(state==STATE_AIR){
+		//print("A");
 		uint64_t timeFromJump = updateTime-(jumpStartTime+JUMP_LAG);
 		uint64_t gameTicksFromJump = timeFromJump/GAME_TICK;
-		if(gameTicksFromJump<0){
-			print("5");
-			return;
-		}
-		if(gameTicksFromJump<=jumpForce){
-			posY=1+gameTicksFromJump;
-		}else{
-			posY=(1+jumpForce)-(gameTicksFromJump-jumpForce); //the max height from jump - fall
+		if(gameTicksFromJump>0){
+			//print("B");
+			if(gameTicksFromJump<=jumpForce){
+				//print("C");
+				posY=1+gameTicksFromJump;
+			}else{
+				//print("D");
+				posY=(1+jumpForce)-(gameTicksFromJump-jumpForce); //the max height from jump - fall
+			}
+			if(posY>VERTICAL_SIZE-3) posY=VERTICAL_SIZE-2;
 			if(posY<=1){
+				//print("G");
 				posY=1;
 				state=STATE_GROUND;	
+				jumpForce=0;
 			} 
 		}
 	}
 
-	if(posY>VERTICAL_SIZE-3) posY=VERTICAL_SIZE-2;
-	if(posY<1) posY=1;
+	//TODO sacar esto y que la logica de antes ande bien.
+	//if(posY>VERTICAL_SIZE-3) posY=VERTICAL_SIZE-2;
+//	if(posY<=1){
+//		posY=1;
+//		state=STATE_GROUND;	
+//		jumpForce=0;
+//	} 
 
 
-
+	//printNum(posY);
 
 	//Obstacle Update
 	uint64_t obstacleTicksFromLastUpdate = ((updateTime-lastObstacleUpdate)/(GAME_TICK*OBSTACLE_LAG_MULTIPLIER))%HORIZONTAL_SIZE;
 	if(obstacleTicksFromLastUpdate>0 && obstacleTicksFromLastUpdate<HORIZONTAL_SIZE){
-		uint64_t newObstaclePosY = time()%VERTICAL_SIZE;
-		printNum(newObstaclePosY);
-		print("\n");
+		uint64_t newObstaclePosY = ((updateTime/13)*7919)%(VERTICAL_SIZE*NO_OBSTACLE_MULTIPLIER);
+		
+		//TODO borrar estas 3 lineas. Debugging. 
+		////printNum(newObstaclePosY);
+		//print("\n");
+		
 		for(uint64_t i=0; i<HORIZONTAL_SIZE; i++){
 			for(uint64_t j =0; j<VERTICAL_SIZE; j++){
 				uint64_t oldI = i+obstacleTicksFromLastUpdate;
@@ -128,13 +146,13 @@ void update(){
 
 				}
 				if(i==0 && (j==posY || j==posY+1 || j==posY-1)){
-					//if(obstacle[i][j]==SPACE_OBSTACLE){
-					//	gameOver();
-					//}else{
+					if(obstacle[i][j]==SPACE_OBSTACLE){
+						gameOver();
+					}else{
 						if(j==posY+1) print("O");
 						if(j==posY  ) print("I");
 						if(j==posY-1) print("N");
-					//
+					}
 				}else{
 					if(obstacle[i][j]==SPACE_OBSTACLE){
 						print("X");
@@ -150,13 +168,13 @@ void update(){
 		for(uint64_t i=0; i<HORIZONTAL_SIZE; i++){
 			for(uint64_t j =0; j<VERTICAL_SIZE; j++){
 				if(i==0 && (j==posY || j==posY+1 || j==posY-1)){
-					//if(obstacle[i][j]==SPACE_OBSTACLE){
-					//	gameOver();
-					//}else{
+					if(obstacle[i][j]==SPACE_OBSTACLE){
+						gameOver();
+					}else{
 						if(j==posY+1) print("O");
 						if(j==posY  ) print("I");
 						if(j==posY-1) print("N");
-					//}
+					}
 				}else{
 					if(obstacle[i][j]==SPACE_OBSTACLE){
 						print("X");
@@ -178,15 +196,17 @@ void update(){
 //TODO use msgQ
 uint64_t isPlayerJumping(){
 	
-	char c = lastUpdateTime%2;
+	char c = ((lastUpdateTime/13)*7919)%2;
 	if(c==1) return 1;
 	else return 0;
 
 }
 
 void gameOver(){
-	for(int i=0; i<100000; i++){
-		print("?");
+	print("\n\n\n Game Over \n rebooting");
+	uint64_t loopTime=time();
+	while(time()<loopTime+GAME_OVER_LAG){
+		
 	}
 	reboot();
 }
