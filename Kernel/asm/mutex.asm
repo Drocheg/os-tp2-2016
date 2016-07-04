262bb5b25c2197cfaecc3da1178ea4ea28ca2dd6
Juan Li Puma <Juan Li Puma>
2016-07-04 12:57:52 -0300

    Removed unneeded LOCKs, XCHG is already atomic

--- Kernel/asm/mutex.asm
+++ Kernel/asm/mutex.asm
@@ -7,6 +7,9 @@ SECTION .text
 ;uint8_t tsl(uint8_t* lockVariable)
 ;This took me WAY too long, appreciate the help from http://stackoverflow.com/questions/22424209/tsl-instruction-reference#comment34099683_22424447
 tsl:
-	LOCK mov al, 1			;LOCK prefix makes these atomic
-	LOCK xchg al, [rdi]		;if al == 0, then lock was set
+	mov al, 1
+	xchg al, [rdi]		;if al == 0, then lock was set
 	ret
+
+;XCGH is atomic - from the Intel manual, Vol. 2A page 3-493:
+;The XCHG instruction always asserts the LOCK# signal regardless of the presence or absence of the LOCK prefix.
